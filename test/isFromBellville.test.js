046b2673d5d11b3ee95458f50e440a01af4d2f36
Khazimla <20113497@rcconnect.co.za>
2022-07-26 14:21:54 +0200

    description changes

--- test/isFromBellville.test.js
+++ test/isFromBellville.test.js
@@ -1,19 +1,19 @@
 let assert = require("assert");
 let isFromBellville = require("../isFromBellville");
 
+
 describe('Test for my Bellville function' , function(){
-    it("it should return 'true' when called with 'CY 457' " , function(){
+    it("it should return true if the registration number starts with 'CY 123' " , function(){
         assert.equal(true,isFromBellville("CY 123"));
 
     });
-    it("it should return 'false' when called with 'CJ 123' " , function(){
+    it("it should return false if the registration number starts with 'CJ 123' " , function(){
         assert.equal(false,isFromBellville("CJ 123 "));
 
     });
-    it("it should return 'true' when called with 'CY' " , function(){
+    it("it should return true if the registration number starts with 'CY 123' " , function(){
         assert.equal(true,isFromBellville("CY 123 "));
 
     });
-
 
 });
