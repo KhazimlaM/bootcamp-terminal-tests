e380e7a7c0cfbcaf2ca07a6ddd6737240c3870c3
Khazimla <20113497@rcconnect.co.za>
2022-07-26 14:45:01 +0200

    description changes

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -58,14 +58,16 @@ var itemList = [
     {name : 'apples', qty : 37}
   ];
 
-describe('Test for my findItemsOver function' , function(){
-    it("it should return 'pears' if the information passed in is 'itemList3' ", function(){
-        assert.deepEqual(results3,findItemsOver(itemList3,20));
-        
-    });
-
-    it("it should return 'pears' if the information passed in is 'itemList2' ", function(){
-        assert.deepEqual(results2, findItemsOver(itemList2,19));
-        
-    });
-});
+  describe('Test for my findItemsOver function' , function(){
+    it("it should return the list of items over '20' ") , function(){
+     
+         assert.deepEqual(results3,findItemsOver(itemList3,20));
+         
+     }
+ 
+     it("it should return the list of items over '19' ", function(){
+         assert.deepEqual(results2, findItemsOver(itemList2,19));
+         
+     });
+ });
+ 
